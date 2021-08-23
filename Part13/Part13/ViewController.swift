55a76497edead53ca75ab7ac6daaf1ea963d869c
higa-ko <85394813+higa-ko@users.noreply.github.com>
2021-08-23 23:04:44 +0900

    Add files via upload

---
+++ Part13/Part13/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Part13
+//
+//  Created by 葡萄酒 on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    private let fruits = ["りんご","みかん","バナナ","パイナップル"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "CheckCell", for: indexPath)
+        
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        if indexPath.row % 2 == 0 {
+            cell.imageView?.tintColor = .white
+        }
+        return cell
+    }
+}
