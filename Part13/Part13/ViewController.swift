fa98da00faf281ce27d4f49a81edb4d3b04141f5
akio0911 <akio0911@gmail.com>
2021-08-25 18:06:56 +0900

    feedback

--- Part13/Part13/ViewController.swift
+++ Part13/Part13/ViewController.swift
@@ -7,26 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    
-    private let fruits = ["りんご","みかん","バナナ","パイナップル"]
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private let items = ["りんご", "みかん", "バナナ", "パイナップル"]
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        fruits.count
+        items.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "CheckCell", for: indexPath)
-        
-        cell.textLabel!.text = fruits[indexPath.row]
-        
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckCell", for: indexPath)
+
+        cell.textLabel?.text = items[indexPath.row]
+
         if indexPath.row % 2 == 0 {
             cell.imageView?.tintColor = .white
         } else {
             cell.imageView?.tintColor = .orange
         }
-        
+
         return cell
     }
 }
